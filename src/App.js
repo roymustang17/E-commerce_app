bc50a4ac3d82a5940706454e45cd13b9057b9b06
nijo-Arjun <arjun@nijoplus.com>
2023-09-09 00:10:08 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,147 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import AppItem from "./AppItem";
 
-function App() {
+const App = () => {
+  const [data, setData] = useState([]);
+  const [sortRequested, setSortRequested] = useState(false);
+  const [itemsToRender, setItemsToRender] = useState([]);
+  // const [filteredData, setFilteredData] = useState([]);
+  const styling = {
+    border: "2px solid red",
+  };
+
+  useEffect(() => {
+    fetchApiData();
+    // console.log("data fetched");
+  }, []);
+
+  const fetchApiData = async () => {
+    try {
+      const res = await fetch("https://fakestoreapi.com/products");
+      const data = await res.json();
+      setData(data);
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    if (sortRequested) {
+      const sorted = [...data].sort((a, b) => b.price - a.price);
+      setItemsToRender(sorted);
+      console.log("data sorted");
+    } else {
+      setItemsToRender(data); // Reset to the original data when sorting is not requested
+    }
+  }, [sortRequested, data]);
+
+  function capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  const filtered = () => {
+    return data.filter((item) => item.category === "women's clothing");
+  };
+
+  const filterData = () => {
+    const filteredData = filtered();
+    setItemsToRender(filteredData);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <div>
+        <button
+          style={styling}
+          onClick={() => setSortRequested(!sortRequested)}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          Press me!
+        </button>
+        <button onClick={filterData}>Try to press me!</button>
+      </div>
+      <div>
+        {itemsToRender.map((items) => {
+          const formatText = capitalize(items.category);
+          return (
+            <div key={items.id}>
+              <AppItem
+                title={items.title}
+                image={items.image}
+                category={formatText}
+                desc={items.description}
+                price={items.price}
+                rating={items.rating.rate}
+              />
+            </div>
+          );
+        })}
+      </div>
+    </>
   );
-}
+};
 
 export default App;
+
+// import React, { useState } from "react";
+
+// const App = () => {
+//   const [email, setEmail] = useState("");
+//   const [password, setPassword] = useState("");
+
+//   const [entry, setEntry] = useState([]);
+
+//   const submitForm = (e) => {
+//     e.preventDefault();
+
+//     const newEntry = { email: email, password: password };
+//     setEntry([...entry, newEntry]);
+//     console.log(entry);
+//   };
+
+//   const styling = { border: "2px solid blue" };
+
+//   return (
+//     <>
+//       <form action="" onSubmit={submitForm}>
+//         <div>
+//           <label htmlFor="email">E-mail</label>
+//           <input
+//             type="text"
+//             name="email"
+//             id="email"
+//             autoComplete="off"
+//             value={email}
+//             onChange={(e) => setEmail(e.target.value)}
+//           />
+//         </div>
+//         <div>
+//           <label htmlFor="password">Password</label>
+//           <input
+//             type="password"
+//             name="password"
+//             id="password"
+//             autoComplete="off"
+//             value={password}
+//             onChange={(e) => setPassword(e.target.value)}
+//           />
+//         </div>
+
+//         <button>Login</button>
+//       </form>
+
+//       <div>
+//         {entry.map((element) => {
+//           return (
+//             <div key={element.password} style={styling}>
+//               <p>{element.email}</p>
+//               <p>{element.password}</p>
+//             </div>
+//           );
+//         })}
+//       </div>
+//     </>
+//   );
+// };
+
+// export default App;
---
+++ src/AppItem.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+
+export class AppItem extends Component {
+  render() {
+    let { title, image, category, desc, price, rating } = this.props;
+    const sizing = { height: "200px", width: "180px" };
+    const fontCategory = {
+      color: "blue",
+      fontSize: "32px",
+      background: "linear-gradient(180deg, #FF5733, #000000)",
+      WebkitBackgroundClip: "text",
+      WebkitTextFillColor: "transparent",
+      fontWeight: "bold",
+    };
+    const styling = {
+      border: "2px solid black",
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      justifyContent: "space-around",
+      background: "linear-gradient(45deg, #07F3F2, #F507DD)",
+    };
+    return (
+      <div style={styling}>
+        <h1>{title}</h1>
+        <img src={image} alt="/" style={sizing} />
+        <p style={fontCategory}>{category}</p>
+        <h3>{desc}</h3>
+        <p>&#8377; {price}</p>
+        <p>
+          &#9733;{": "}
+          {rating}
+        </p>
+      </div>
+    );
+  }
+}
+
+export default AppItem;
